b29637818127086f137a509d0552bfb148f88d6a
Furu222 <m.furudate222@gmail.com>
2019-07-21 05:39:12 +0900

    :sparkles: add commit template
    gitmoji: https://gitmoji.carloscuesta.me/
    Emoji: https://www.webfx.com/tools/emoji-cheat-sheet/
    Emojipedia: https://emojipedia.org/

--- setup.sh
+++ setup.sh
@@ -1,8 +1,21 @@
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.gvimrc ~/.gvimrc
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.inputrc ~/.inputrc
+#!/bin/bash
+
+wd=$(dirname $0) # working directory(default: ~/dotfiles)
+ctdir=${wd}/commit_template # if change directory name, change .gitconfig.
+
+ln -s ${wd}/.bash_profile ~/.bash_profile
+ln -s ${wd}/.bashrc ~/.bashrc
+ln -s ${wd}/.gvimrc ~/.gvimrc
+ln -s ${wd}/.tmux.conf ~/.tmux.conf
+ln -s ${wd}/.vim ~/.vim
+ln -s ${wd}/.vimrc ~/.vimrc
+ln -s ${wd}/.gitconfig ~/.gitconfig
+ln -s ${wd}/.inputrc ~/.inputrc
+
+# set & refresh git commit template
+if [ ! -d  ${ctdir} ]; then
+  git clone https://gist.github.com/24e2ccd109b7b6a2f20dff7485b24f56.git ${ctdir}
+fi
+
+cd ${ctdir}
+git pull origin master
